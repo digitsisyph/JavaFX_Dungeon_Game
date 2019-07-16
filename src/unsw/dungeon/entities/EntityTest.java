f48d91b1ef14030c1ae558a636661abe7738038a
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-07-16 15:06:35 +1000

    junit working

--- src/unsw/dungeon/entities/EntityTest.java
+++ src/unsw/dungeon/entities/EntityTest.java
@@ -7,9 +7,17 @@ import org.junit.jupiter.api.Test;
 
 class EntityTest {
 
+	int arr[] = { 1, 3 };
+	int arr2[] = { 1, 3 };
+
 	@Test
 	void test() {
 		assertEquals("hello", 1, 1);
+	}
+
+	@Test
+	void testArray() {
+		assertArrayEquals(arr, arr2);
 	}
 
 }
