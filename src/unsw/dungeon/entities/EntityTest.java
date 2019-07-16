61d5123cae8946c8611804f7152d3cbf0c4a5698
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-07-16 14:50:21 +1000

    Added junit jar

---
+++ src/unsw/dungeon/entities/EntityTest.java
@@ -0,0 +1,15 @@
+package unsw.dungeon.entities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EntityTest {
+
+	@Test
+	void test() {
+		assertEquals("hello", 1, 1);
+	}
+
+}
