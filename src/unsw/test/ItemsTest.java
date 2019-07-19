b121b433b60a76d0fe53606b909fedb1c648e6bd
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-07-20 01:30:37 +1000

    problem with frontend

--- src/unsw/test/ItemsTest.java
+++ src/unsw/test/ItemsTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import unsw.dungeon.model.entities.Enemy;
+import unsw.dungeon.model.entities.EntityType;
 import unsw.dungeon.model.entities.Potion;
+import unsw.dungeon.model.entities.Bomb.LitBomb;
+import unsw.dungeon.model.entities.Bomb.UnlitBomb;
 
 public class ItemsTest extends testSetup {
 
@@ -24,7 +27,7 @@ public class ItemsTest extends testSetup {
 		player.moveRight();
 		assertEquals(distance, (enemy.getX() - player.getX()));
 	}
-	
+
 	@Test
 	void testEnemyMoveTowards() {
 		setup(10, 1, 0, 0);
@@ -34,18 +37,33 @@ public class ItemsTest extends testSetup {
 		player.moveRight(); // pick potion
 		assertEquals(distance - 2, (enemy.getX() - player.getX()));
 		player.moveRight();
-		assertEquals(distance - 4 , (enemy.getX() - player.getX()));
+		assertEquals(distance - 4, (enemy.getX() - player.getX()));
 		player.moveRight();
-		assertEquals(distance - 6 , (enemy.getX() - player.getX()));
+		assertEquals(distance - 6, (enemy.getX() - player.getX()));
 	}
-	
+
 	@Test
-	void testLitBomb() {
-		
+	void testLitBombStates() {
+		setup(10, 1, 0, 0);
+		UnlitBomb bomb = new UnlitBomb(1, 0, dungeon);
+		dungeon.addEntity(bomb);
+		player.moveRight();
+		player.moveRight();
+		player.moveRight();
+		player.moveRight();
+		player.placeBomb();
+		LitBomb bombLit = (LitBomb) dungeon.getEntities(EntityType.LITBOMB).get(0);
+		assertEquals("/bomb_lit_1.png", bombLit.getImagePath());
+		player.moveLeft();
+		assertEquals("/bomb_lit_2.png", bombLit.getImagePath());
+		player.moveLeft();
+		assertEquals("/bomb_lit_3.png", bombLit.getImagePath());
+		player.moveLeft();
+		assertEquals("/bomb_lit_4.png", bombLit.getImagePath());
 	}
-	
+
 	@Test
 	void testExplodedBombDestroyEntities() {
-		
+
 	}
 }
