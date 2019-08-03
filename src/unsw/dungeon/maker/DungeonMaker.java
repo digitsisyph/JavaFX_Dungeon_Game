e94fb9e656aaa650eff1abc8be4963db97978db5
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-08-04 00:44:08 +1000

    fixed config

--- src/unsw/dungeon/maker/DungeonMaker.java
+++ src/unsw/dungeon/maker/DungeonMaker.java
@@ -1,5 +1,8 @@
 package unsw.dungeon.maker;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -15,6 +18,7 @@ import javafx.beans.property.StringProperty;
 public class DungeonMaker {
 
 	private StringProperty[][] cell;
+	private StringProperty _goal;
 	private int keyID = 0;
 	private int doorID = 0;
 
@@ -25,9 +29,23 @@ public class DungeonMaker {
 				cell[i][j] = new SimpleStringProperty();
 			}
 		}
+		_goal = new SimpleStringProperty();
 	}
 
-	public void create() {
+	public void writeToFile(String filename) {
+		String path = "dungeons/" + filename;
+		JSONObject obj = createJSONObject();
+		try {
+			FileWriter file = new FileWriter(path);
+			file.write(obj.toString(2));
+			System.out.println("Successfully Copied JSON Object to File...");
+			file.close();
+		} catch (IOException e) {
+			System.err.println("Something went wrong");
+		}
+	}
+
+	private JSONObject createJSONObject() {
 		resetID();
 		JSONObject obj = new JSONObject();
 		obj.put("width", 15);
@@ -55,15 +73,48 @@ public class DungeonMaker {
 			}
 		}
 		obj.put("entities", ent_list);
+		
+		JSONObject goal = new JSONObject();
+		obj.put("goal-condition", goal.put("goal", _goal.getValue()));
+		
 		System.out.println(obj.toString(2));
+		return obj;
+	}
+
+	public void clearDungeon() {
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				cell[i][j].setValue(null);
+			}
+		}
+	}
+
+	public void addSurroundingWalls() {
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				if (i == 0)
+					cell[i][j].setValue("wall");
+				if (i == 14)
+					cell[i][j].setValue("wall");
+				if (j == 0)
+					cell[i][j].setValue("wall");
+				if (j == 14)
+					cell[i][j].setValue("wall");
+			}
+		}
 	}
 
 	private void resetID() {
 		keyID = 0;
 		doorID = 0;
 	}
+
 	public StringProperty cellProperty(int x, int y) {
 		return cell[x][y];
+	}
+	
+	public StringProperty getGoalProperty() {
+		return _goal;
 	}
 
 }
